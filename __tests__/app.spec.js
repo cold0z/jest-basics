eb3a5247d0964b7fb0cd4b63063d8e660a755a26
Jihad Haddou <101198192+webenfly1@users.noreply.github.com>
2023-05-20 21:10:59 +0100

    ✅ Add test using spyOn to verify function calls

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -1,6 +1,14 @@
-const {getData} = require('./app');
+const {getData} = require('../app');
+const crypto = require('crypto');
+
+//jest.mock('crypto');
 
 test("Fetch data", async()=> {
+
+    jest.spyOn(crypto, "randomBytes").mockResolvedValueOnce("bytes-test");
+
     const res = await getData();
     console.log(res)
+    expect(res).toBe("bytes-test");
+
 })
